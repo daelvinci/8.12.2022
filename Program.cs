2a19c64ec7016d6f9df510246ee1ba2ffa3ce732
Elvin A. Bilalov <elvinab@code.local>
2022-12-09 19:22:17 +0400

    Reqem cemi

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin!");
+            string numstr = Console.ReadLine();
+            int num = Convert.ToInt32(numstr);
+            int a;
+            int sum = 0;
+
+            for(int i=0; i<numstr.Length; i++)
+            {
+                if(num>0)
+                {
+                a = num % 10;
+                num = (num - a) / 10;
+                sum += a;
+                }
+
+               
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
