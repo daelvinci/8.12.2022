1fca0c49947c8431a71b8ff783f938368528bf63
daelvinci <119412720+daelvinci@users.noreply.github.com>
2022-12-09 21:25:30 +0400

    erorrlu olan cs3

---
+++ Program3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -Verilmiş yazıda rəqəm olmamasını yoxlayan proqram, əgər rəqəm varsa yenidən yazının daxil edilməsini istəməlidir ta ki rəqəmsiz daxil edilənədək.
+
+            Console.WriteLine("Yazini daxil edin");
+            string yazi = Console.ReadLine();
+           
+            string nums= "1234567890";
+
+            int j = 0;
+
+            int i = 0;
+
+            bool has = false;
+
+            for (j < yazi.Length; j++;)
+            {
+
+                if (yazi[j]==nums[i])
+                {
+                    has = true;
+                }
+
+                else
+                {
+                    for (i < nums.Length; i++;)
+                    {
+                        if (yazi[j] == nums[i])
+                        {
+                            has = true;
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+            if (has==false)
+            {
+                Console.WriteLine("Duzdur");
+            }
+            else
+            {
+                Console.WriteLine("Duz Yazi daxil edin!");
+            }
+
+
+
+        }
+    }
+}
+
+
